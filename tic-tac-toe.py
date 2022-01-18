4cede1d944803eade978635309b412f70b853ea8
Edwin Adjei <code_ed381@outlook.com>
2022-01-18 13:48:07 +0000

    Program completed

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,8 +1,9 @@
 import tkinter
 from tkinter import *
+from tkinter import Menu
 from tkinter.constants import DISABLED, NORMAL
 
-root = Tk()
+root = tkinter.Tk()
 
 root.geometry('355x400')
 
@@ -10,20 +11,68 @@ root.title('Tic-Tac-Toe')
 
 root.configure(bg='#36454F')
 
-menu_widget = tkinter.Menu(root)
-
-submenu_widget = tkinter.Menu(menu_widget, tearoff=False)
-
-menu_widget.add_cascade(label="Options")
-
-menu_widget.add_command(label="Help")
-
-root.config(menu = menu_widget)
 
 
+def Help():
+    howToTxt = " You probably already know how to play Tic-Tac-Toe. \nIt's a really simple game, right? That's what most people think. \nBut if you really wrap your brain around it, you'll discover that \nTic-Tac-Toe isn't quite as simple as you think! \nTic-Tac -Toe (along with a lot of other games) involves looking ahead and \ntrying to figure out what the person playing against you might do next. "
+    rulesTxt = " 1. The game is played on a grid that's 3 squares by 3 squares.\n\n 2. You are X, your opponent is O.\n Players take turns putting their marks in empty squares.\n\n 3. The first player to get 3 of her marks in a row \n(up, down, across, or diagonally) is the winner. \n\n4. When all 9 squares are full, the game is over. \nIf no player has 3 marks in a row, the game ends in a tie. "
+    help = Toplevel(master=None)
+    help.title("Help")
+    help.geometry('450x400')
+    help.config(bg='white')
+
+    howToPlay = tkinter.Label(help, text='How to play', bg='white', font='Arial 14')
+    howToPlay.pack()
+    howTo = tkinter.Label(help, text=howToTxt, bg='white', font='Arial 10')
+    howTo.pack()
+
+    rules = tkinter.Label(help, text='\nRules of the game', bg='white', font='Arial 14')
+    rules.pack()
+    rulesText = tkinter.Label(help, text=rulesTxt, bg='white', font='Arial 10')
+    rulesText.pack()
+
+#Menu commands and widgets for newgame, exit and help
+def MenuCmd():
+    r1c1['state'] = tkinter.NORMAL
+    r1c2['state'] = tkinter.NORMAL
+    r1c3['state'] = tkinter.NORMAL
+    r2c1['state'] = tkinter.NORMAL
+    r2c2['state'] = tkinter.NORMAL
+    r2c3['state'] = tkinter.NORMAL
+    r3c1['state'] = tkinter.NORMAL
+    r3c2['state'] = tkinter.NORMAL
+    r3c3['state'] = tkinter.NORMAL
+
+    row1col1.set('')
+    row1col2.set('')
+    row1col3.set('')
+    row2col1.set('')
+    row2col2.set('')
+    row2col3.set('')
+    row3col1.set('')
+    row3col2.set('')
+    row3col3.set('')
+
+    playerOneBtn['state'] = tkinter.NORMAL
+    playerOneBtn['bg'] = 'green'
+    playerOneBtn['fg'] = 'white'
+
+    playerTwoBtn['state'] = tkinter.DISABLED
+    playerTwoBtn['bg'] = 'red'
+    playerTwoBtn['fg'] = 'black'
+
+menu_widget = Menu(root)
+root.config(menu=menu_widget)
+
+submenu_widget = Menu(menu_widget, tearoff=FALSE)
+submenu_widget.add_command(label='New game', command=MenuCmd)
+submenu_widget.add_command(label='Exit', command=lambda: root.destroy())
+
+menu_widget.add_cascade(label="Options", menu=submenu_widget)
+menu_widget.add_command(label="Help", command=Help)
 
 
-
+#Changes the state of player buttons from Normal to Disabled and vice versa when game buttons are clicked
 def changeState():
     if (playerOneBtn['state'] == tkinter.NORMAL) :
         playerOneBtn['state'] = tkinter.DISABLED
@@ -43,14 +92,15 @@ def changeState():
         playerTwoBtn['bg'] = 'red'
         playerTwoBtn['fg'] = 'black'
 
+#Player buttons, shows the next and current player
 playerOneBtn = tkinter.Button(root, text='Player 1 (X)', fg='white', bg='green', font="Arial 10", state=NORMAL)
 playerOneBtn.place(x=80, y=40)
-
 
 playerTwoBtn = tkinter.Button(root, text='Player 2 (O)', fg='black', bg='red',font="Arial 10", state=DISABLED)
 playerTwoBtn.place(x=185, y=40)
 
 
+#String variables of game buttons, grabs the values of the buttons for further manipulation
 row1col1 = StringVar()
 row1col2 = StringVar()
 row1col3 = StringVar()
@@ -62,7 +112,7 @@ row3col2 = StringVar()
 row3col3 = StringVar()
 
 
-
+#Restarts the game when new game is pressed
 def Restart(winMessageFxn):
     r1c1['state'] = tkinter.NORMAL
     r1c2['state'] = tkinter.NORMAL
@@ -89,7 +139,7 @@ def Restart(winMessageFxn):
     changeState()
     
     
-
+#Opens another tkinter window to display the results of the game
 def Winner(winner):
     if winner == 'Player 1':
         winMessage = Toplevel(master=None)
@@ -97,7 +147,7 @@ def Winner(winner):
         winMessage.geometry('300x200')
         winMessage.config(bg='white')
 
-        winnerText = tkinter.Label(winMessage,text='Player one won the game', bg='white',font='Calibri 12')
+        winnerText = tkinter.Label(winMessage,text='Player one won the game', bg='white',fg='green', font='Calibri 12')
         winnerText.place(x=42, y=30)
         newgame = tkinter.Button(winMessage, text='New game', font='Arial 12', command= lambda : Restart(winMessage))
         newgame.place(x=100, y=65)
@@ -110,7 +160,7 @@ def Winner(winner):
         winMessage.geometry('300x200')
         winMessage.config(bg='white')
 
-        winnerText = tkinter.Label(winMessage,text='Player two won the game', bg='white', font='Calibri 12')
+        winnerText = tkinter.Label(winMessage,text='Player two won the game', bg='white', fg='red', font='Calibri 12')
         winnerText.place(x=42, y=30)
         newgame = tkinter.Button(winMessage, text='New game', font='Arial 12', command= lambda : Restart(winMessage))
         newgame.place(x=100, y=65)
@@ -131,14 +181,14 @@ def Winner(winner):
                                                 winMessage.geometry('300x200')
                                                 winMessage.config(bg='white')
 
-                                                winnerText = tkinter.Label(winMessage,text="It's a draw game", bg='white', font='Calibri 12')
-                                                winnerText.place(x=42, y=30)
+                                                winnerText = tkinter.Label(winMessage,text="It's a draw game", bg='white', fg='blue', font='Calibri 12')
+                                                winnerText.place(x=80, y=30)
                                                 newgame = tkinter.Button(winMessage, text='New game', font='Arial 12', command= lambda : Restart(winMessage))
                                                 newgame.place(x=100, y=65)
                                                 newgame = tkinter.Button(winMessage, text='Exit', font='Arial 12', command = lambda: root.destroy())
                                                 newgame.place(x=127, y=105)
 
-winner = ''
+#Game buttons functions from button to button nine
 def btnOneCmd(x, y):
     if playerOneBtn['state'] == tkinter.NORMAL:
         row1col1.set(x)
@@ -153,6 +203,9 @@ def btnOneCmd(x, y):
         Winner(winner)
     if (row1col1.get() == y and row1col2.get() == y and row1col3.get() == y) or (row1col1.get() == y and row2col2.get() == y and row3col3.get() == y) or (row1col1.get() == y and row2col1.get() == y and row3col1.get() == y):
         winner = 'Player 2'
+        Winner(winner)
+    else:
+        winner = ''
         Winner(winner)
 
 def btnTwoCmd(x, y):
@@ -308,7 +361,7 @@ def btnNineCmd(x, y):
         Winner(winner)
 
 
-
+#Game buttons
 r1c1= tkinter.Button(root, textvariable=row1col1, font="Arial 20",bg='#F4BB44', width='3', height='2', command=lambda: btnOneCmd("X", "O"), state= tkinter.NORMAL)
 r1c1.place(x=70, y=100)
 
