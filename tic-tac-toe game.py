9662a826bd5a1013516c0b4e57ca7f61a39649de
Edwin Adjei <code_ed381@outlook.com>
2022-01-25 16:00:05 +0000

    Finished win function

--- tic-tac-toe game.py
+++ tic-tac-toe game.py
@@ -23,97 +23,229 @@ def changeStateTwo():
         oButton['bg'] = 'black'
         oButton['fg'] = 'white'
 
+btOne = ''
+btTwo = ''
+btThree = ''
+btFour = ''
+btFive = ''
+btSix = ''
+btSeven = ''
+btEight = ''
+btNine = ''
 
 def boxOne(event):
-    if xButton['state'] == tkinter.NORMAL:
+    global btOne
+    if xButton['state'] == NORMAL:
         btOne = box1.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
         box1.unbind('<Button-1>')
-    elif xButton['state'] == tkinter.DISABLED:
+    elif xButton['state'] == DISABLED:
         btOne = box1.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box1.unbind('<Button-1>')
+    win()
 
 def boxTwo(event):
+    global btTwo
     if xButton['state'] == tkinter.NORMAL:
-        box2.create_text(40,40,text="x",font="Arial 72")
+        btTwo = box2.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
-        box2.unbind('<Button-1>')
+        box2.unbind('<Button-1>')        
     elif xButton['state'] == tkinter.DISABLED:
-        box2.create_text(40,40,text="o",font="Arial 72")
+        btTwo = box2.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box2.unbind('<Button-1>')
+    win()
 
 def boxThree(event):
+    global btThree
     if xButton['state'] == tkinter.NORMAL:
-        box3.create_text(40,40,text="x",font="Arial 72")
+        btThree = box3.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
         box3.unbind('<Button-1>')
     elif xButton['state'] == tkinter.DISABLED:
-        box3.create_text(40,40,text="o",font="Arial 72")
+        btThree = box3.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box3.unbind('<Button-1>')
+    win()
 
 def boxFour(event):
+    global btFour
     if xButton['state'] == tkinter.NORMAL:
-        box4.create_text(40,40,text="x",font="Arial 72")
+        btFour = box4.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
         box4.unbind('<Button-1>')
     elif xButton['state'] == tkinter.DISABLED:
-        box4.create_text(40,40,text="o",font="Arial 72")
+        btFour = box4.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box4.unbind('<Button-1>')
+    win()
 
 def boxFive(event):
+    global btFive
     if xButton['state'] == tkinter.NORMAL:
-        box5.create_text(40,40,text="x",font="Arial 72")
+        btFive = box5.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
         box5.unbind('<Button-1>')
     elif xButton['state'] == tkinter.DISABLED:
-        box5.create_text(40,40,text="o",font="Arial 72")
+        btFive = box5.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box5.unbind('<Button-1>')
+    win()
 
 def boxSix(event):
+    global btSix
     if xButton['state'] == tkinter.NORMAL:
-        box6.create_text(40,40,text="x",font="Arial 72")
+        btSix = box6.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
         box6.unbind('<Button-1>')
     elif xButton['state'] == tkinter.DISABLED:
-        box6.create_text(40,40,text="o",font="Arial 72")
+        btSix = box6.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box6.unbind('<Button-1>')
+    win()
 
 def boxSeven(event):
+    global btSeven
     if xButton['state'] == tkinter.NORMAL:
-        box7.create_text(40,40,text="x",font="Arial 72")
+        btSeven = box7.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
         box7.unbind('<Button-1>')
     elif xButton['state'] == tkinter.DISABLED:
-        box7.create_text(40,40,text="o",font="Arial 72")
+        btSeven = box7.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box7.unbind('<Button-1>')
+    win()
 
 def boxEight(event):
+    global btEight
     if xButton['state'] == tkinter.NORMAL:
-        box8.create_text(40,40,text="x",font="Arial 72")
+        btEight = box8.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
         box8.unbind('<Button-1>')
     elif xButton['state'] == tkinter.DISABLED:
-        box8.create_text(40,40,text="o",font="Arial 72")
+        btEight = box8.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box8.unbind('<Button-1>')
+    win()
 
 def boxNine(event):
+    global btNine
     if xButton['state'] == tkinter.NORMAL:
-        box9.create_text(40,40,text="x",font="Arial 72")
+        btNine = box9.create_text(40,40,text="x",font="Arial 72")
         changeStateOne()
         box9.unbind('<Button-1>')
     elif xButton['state'] == tkinter.DISABLED:
-        box9.create_text(40,40,text="o",font="Arial 72")
+        btNine = box9.create_text(40,40,text="o",font="Arial 72")
         changeStateTwo()
         box9.unbind('<Button-1>')
+    win()
+
+def Restart(newTopLevel):
+    xButton['state'] = NORMAL
+    oButton['state'] = DISABLED
+
+    box1.create_text(40,40,text="",font="Arial 72")
+    box1.bind('<Button-1>', boxOne)
+    newTopLevel.destroy()
+
+def Winner(winner):
+    if winner == 'player1':
+        newwindow = Toplevel(master=None)
+        newwindow.title('Player One Wins')
+        newwindow.geometry('320x200')
+        newwindow.config(bg='white')
+
+        winMessage = Label(newwindow, text='Player One Wins the game', bg='white', font='Calibri 14')
+        winMessage.place(x=40, y=25)
+        newGame = Button(newwindow, text='New game',width='10', font='Arial 14', command=lambda: Restart(newwindow))
+        newGame.place(x=100, y=65)
+        exit = Button(newwindow, text='Exit',width='10', font='Arial 14', command=lambda: window.destroy())
+        exit.place(x=100, y=105)
+
+    if winner == 'player2':
+        newwindow = Toplevel(master=None)
+        newwindow.title('Player One Wins')
+        newwindow.geometry('320x200')
+        newwindow.config(bg='white')
+
+        winMessage = Label(newwindow, text='Player Two Wins the game', bg='white', font='Calibri 14')
+        winMessage.place(x=40, y=25)
+        newGame = Button(newwindow, text='New game',width='10', font='Arial 14', command=lambda: Restart(newwindow))
+        newGame.place(x=100, y=65)
+        exit = Button(newwindow, text='Exit',width='10', font='Arial 14', command=lambda: window.destroy())
+        exit.place(x=100, y=105)
     
+    if winner == 'draw':
+        newwindow = Toplevel(master=None)
+        newwindow.title('Player One Wins')
+        newwindow.geometry('320x200')
+        newwindow.config(bg='white')
+
+        winMessage = Label(newwindow, text='Its a tie', bg='white', font='Calibri 14')
+        winMessage.place(x=40, y=25)
+        newGame = Button(newwindow, text='New game',width='10', font='Arial 14', command=lambda: Restart(newwindow))
+        newGame.place(x=100, y=65)
+        exit = Button(newwindow, text='Exit',width='10', font='Arial 14', command=lambda: window.destroy())
+        exit.place(x=100, y=105)
+
+
+
+def win():
+    if box1.itemcget(btOne, 'text') == 'x' and box2.itemcget(btTwo, 'text') == 'x' and box3.itemcget(btThree, 'text') == 'x':
+        winner = 'player1'
+        Winner(winner)
+    elif box1.itemcget(btOne, 'text') == 'o' and box2.itemcget(btTwo, 'text') == 'o' and box3.itemcget(btThree, 'text') == 'o':
+        winner = 'player2'
+        Winner(winner)
+    elif box4.itemcget(btFour, 'text') == 'x' and box5.itemcget(btFive, 'text') == 'x' and box6.itemcget(btSix, 'text') == 'x':
+        winner = 'player1'
+        Winner(winner)
+    elif box4.itemcget(btFour, 'text') == 'o' and box5.itemcget(btFive, 'text') == 'o' and box6.itemcget(btSix, 'text') == 'o':
+        winner = 'player2'
+        Winner(winner)
+    elif box7.itemcget(btSeven, 'text') == 'x' and box8.itemcget(btEight, 'text') == 'x' and box9.itemcget(btNine, 'text') == 'x':
+        winner = 'player1'
+        Winner(winner)
+    elif box7.itemcget(btSeven, 'text') == 'o' and box8.itemcget(btEight, 'text') == 'o' and box9.itemcget(btNine, 'text') == 'o':
+        winner = 'player2'
+        Winner(winner)
+    elif box1.itemcget(btOne, 'text') == 'x' and box4.itemcget(btFour, 'text') == 'x' and box7.itemcget(btSeven, 'text') == 'x':
+        winner = 'player1'
+        Winner(winner)
+    elif box1.itemcget(btOne, 'text') == 'o' and box4.itemcget(btFour, 'text') == 'o' and box7.itemcget(btSeven, 'text') == 'o':
+        winner = 'player2'
+        Winner(winner)
+    elif box2.itemcget(btTwo, 'text') == 'x' and box5.itemcget(btFive, 'text') == 'x' and box8.itemcget(btEight, 'text') == 'x':
+        winner = 'player1'
+        Winner(winner)
+    elif box2.itemcget(btTwo, 'text') == 'o' and box5.itemcget(btFive, 'text') == 'o' and box8.itemcget(btEight, 'text') == 'o':
+        winner = 'player2'
+        Winner(winner)
+    elif box3.itemcget(btThree, 'text') == 'x' and box6.itemcget(btSix, 'text') == 'x' and box9.itemcget(btNine, 'text') == 'x':
+        winner = 'player1'
+        Winner(winner)
+    elif box3.itemcget(btThree, 'text') == 'o' and box6.itemcget(btSix, 'text') == 'o' and box9.itemcget(btNine, 'text') == 'o':
+        winner = 'player2'
+        Winner(winner)
+    elif box1.itemcget(btOne, 'text') == 'x' and box5.itemcget(btFive, 'text') == 'x' and box9.itemcget(btNine, 'text') == 'x':
+        winner = 'player1'
+        Winner(winner)
+    elif box1.itemcget(btOne, 'text') == 'o' and box5.itemcget(btFive, 'text') == 'o' and box9.itemcget(btNine, 'text') == 'o':
+        winner = 'player2'
+        Winner(winner)
+    elif box3.itemcget(btThree, 'text') == 'x' and box5.itemcget(btFive, 'text') == 'x' and box7.itemcget(btSeven, 'text') == 'x':
+        winner = 'player1'
+        Winner(winner)
+    elif box3.itemcget(btThree, 'text') == 'o' and box5.itemcget(btFive, 'text') == 'o' and box7.itemcget(btSeven, 'text') == 'o':
+        winner = 'player2'
+        Winner(winner)
+    elif box1.itemcget(btOne, 'text') != '' and box2.itemcget(btTwo, 'text') != '' and box3.itemcget(btThree, 'text') != '' and box4.itemcget(btFour, 'text') != '' and box5.itemcget(btFive, 'text') != '' and box6.itemcget(btSix, 'text') != '' and box7.itemcget(btSeven, 'text') != '' and box8.itemcget(btEight, 'text') != '' and box9.itemcget(btNine, 'text') != '':
+        winner = 'draw'
+        Winner(winner)
+
+
+
 
 canvas = tkinter.Canvas(window,bg="white",highlightthickness=0)
 canvas.place(x=10,y=20,width=270)
